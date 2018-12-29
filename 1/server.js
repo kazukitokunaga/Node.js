596c033c6a10d2671f222c669fd50d7ddfedd2c9
kazukitokunaga <tokunagakazuki330@gmail.com>
2018-12-29 12:32:08 +0900

    クエリストリングを解析する

--- 1/server.js
+++ 1/server.js
@@ -1,25 +1,24 @@
 var http = require('http');
+var url = require('url');
 // pages配列を定義
 var pages = [
-  {route: '/', output: 'シンプルなサンプルコードです。'},
-  {route: '/about/this', output: 'Nodeで複数階層ルーティング'},
-  {route: '/about/node', output: 'V8エンジンのためのイベントI/O'},
-  {route: '/another page', output: function(){return 'これが' + this.route;}}
+  // http://localhost:8080/?id=1のようにURLにクエリストリングを追加することによって、該当するidプロパティを持ったコンテンツにアクセスすることができる。
+  {id: '1', route: '', output: 'WooHoo!'},
+  {id: '2', route: 'about', output: 'シンプルなサンプルコードです。'},
+  {id: '3', route: 'another page', output: function () {return 'これが' + this.route;}},
 ];
 http.createServer(function (request, response) {
-  // pathのbasenameではなく、request.urlを渡す
-  var lookup = decodeURI(request.url);
-  // pages配列のrouteプロパティの値がlookup変数の値にマッチするかどうかを判定。
-  pages.forEach(function(page){
-    if (page.route === lookup){
-      // マッチした場合はそのオブジェクトのoutputプロパティの値を返す
-      response.writeHead(200, {'Content-type': 'text/html'});
-      response.end(typeof page.output === 'function' ? page.output() : page.output);
-    }
-  });
-  // マッチしない場合は404を返す
+  var id = url.parse(decodeURI(request.url), true).query.id;
+  if (id){
+    pages.forEach(function(page){
+      if(page.id === id){
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(typeof page.output === 'function' ? page.output() : page.output);
+      }
+    });
+  }
   if (!response.finished){
     response.writeHead(404);
-    response.end('ページが見つかりません！')
+    response.end('ページが見つかりません！');
   }
 }).listen(8080);
