363e50dd8e4f65ceed453bb84b33829d47e9f166
kazukitokunaga <tokunagakazuki330@gmail.com>
2018-12-29 11:57:02 +0900

    URLルーティングの設定

--- 1/server.js
+++ 1/server.js
@@ -1,5 +1,25 @@
 var http = require('http');
+var path = require('path');
+// pages配列を定義
+var pages = [
+  {route: '', output: 'シンプルなサンプルコードです。'},
+  {route: 'about', output: 'シンプルなサンプルコードです。'},
+  {route: 'another page', output: function(){return 'これが' + this.route;}}
+];
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end('Woohoo!');
+  // pathのbasenameを抽出し、クライアントから送信されたURIをデコード
+  var lookup = path.basename(decodeURI(request.url));
+  // pages配列のrouteプロパティの値がlookup変数の値にマッチするかどうかを判定。
+  pages.forEach(function(page){
+    if (page.route === lookup){
+      // マッチした場合はそのオブジェクトのoutputプロパティの値を返す
+      response.writeHead(200, {'Content-type': 'text/html'});
+      response.end(typeof page.output === 'function' ? page.output() : page.output);
+    }
+  });
+  // マッチしない場合は404を返す
+  if (!response.finished){
+    response.writeHead(404);
+    response.end('ページが見つかりません！')
+  }
 }).listen(8080);
