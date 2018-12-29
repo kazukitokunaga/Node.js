483bd0f9c5b3444c6ef8cd7baaf7a0c595292ecd
kazukitokunaga <tokunagakazuki330@gmail.com>
2018-12-29 12:10:12 +0900

    複数階層コンテンツのルーティング

--- 1/server.js
+++ 1/server.js
@@ -1,14 +1,14 @@
 var http = require('http');
-var path = require('path');
 // pages配列を定義
 var pages = [
-  {route: '', output: 'シンプルなサンプルコードです。'},
-  {route: 'about', output: 'シンプルなサンプルコードです。'},
-  {route: 'another page', output: function(){return 'これが' + this.route;}}
+  {route: '/', output: 'シンプルなサンプルコードです。'},
+  {route: '/about/this', output: 'Nodeで複数階層ルーティング'},
+  {route: '/about/node', output: 'V8エンジンのためのイベントI/O'},
+  {route: '/another page', output: function(){return 'これが' + this.route;}}
 ];
 http.createServer(function (request, response) {
-  // pathのbasenameを抽出し、クライアントから送信されたURIをデコード
-  var lookup = path.basename(decodeURI(request.url));
+  // pathのbasenameではなく、request.urlを渡す
+  var lookup = decodeURI(request.url);
   // pages配列のrouteプロパティの値がlookup変数の値にマッチするかどうかを判定。
   pages.forEach(function(page){
     if (page.route === lookup){
