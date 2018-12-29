e394876c90699da49e7ee9e6eece24d111fb24c5
kazukitokunaga <tokunagakazuki330@gmail.com>
2018-12-29 21:54:00 +0900

    記載するファイル間違い修正

--- 1/1.5/preudosafe_server.js
+++ 1/1.5/preudosafe_server.js
@@ -4,11 +4,21 @@ var url = require('url');
 var fs = require('fs');
 
 http.createServer(function(request, response){
+  // path.basenameではなく、url.parseでurlを取得
   var lookup = url.parse(decodeURI(request.url)).pathname;
-  lookup = (lookup === "/") ? '/indedx.html' : lookup;
-  var f = 'content' + lookup;
+  // url.parseでurlを取得する場合は、path.normalizeでクリーンにしておくことで、悪意ある第三者からの攻撃（相対ディレクトリトラバーサルやヌルバイト攻撃）から守ることができる。
+  lookup = path.normalize(lookup);
+  lookup = (lookup === "/") ? '/indedx.html-serve' : lookup + '-serve';
+  var f = 'content-pseudosafe' + lookup;
   console.log(f);
   fs.readFile(f, function(err, data){
     response.end(data);
+  });
+  fs.exists(f, function(exists){
+    if(!exists){
+      response.writeHead(404);
+      response.end();
+      return;
+    }
   });
 }).listen(8080);
