70424d75c948da54ab30c1a36a2dcf666f17ed0b
kazukitokunaga <tokunagakazuki330@gmail.com>
2018-12-29 15:17:18 +0900

    スタティックファイルを配信する

---
+++ 1/1.2/server.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+// createServerのコールバック関数の外に宣言すると、リクエストされたファイルが存在する場合はpath.extnameで取得したファイルの拡張子をmimeTypesのオブジェクトで検索し、得られたContent-Typeの値をresponse.writeHeadに渡す。
+var mimeTyopes = {
+  '.js': 'text/javascript',
+  '.html': 'text/html',
+  'css': 'text/css'
+};
+http.createServer(function (request, response) {
+  // サーバーで探すファイル名を格納するlookup変数を宣言
+  var lookup = path.basename(decodeURI(request.url)) || 'index.html',
+  // 上記で格納したbasenameにcontent/を付与する。
+  f = 'content/' + lookup;
+  // ファイルが存在するか判定
+  fs.exists(f, function(exists){
+    if(exists){
+      // ファイルが存在する場合は、fs.readFileを非同期で読み込む。
+      fs.readFile(f, function(err, data){
+        if(err){  // エラーハンドリング
+          response.writeHead(500);
+          response.end('Server Error!');
+          return;
+        }
+        var headers = {'contentTyoe' : mimeTyopes[path.extname(f)]};
+        response.writeHead(200, headers);
+        response.end(data);
+      });
+      // fs.readFileメソッドの外にreturnを置くことで、fs.existsがtrueを返した後に404レスポンスの生成等を行わないようにしている。
+      return;
+    }
+    response.writeHead(404);
+    response.end('ページが見つかりません。');
+  });
+}).listen(8080);
