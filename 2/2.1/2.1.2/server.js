4b2bdb8e385165c08175469594bf724ddde89882
kazukitokunaga <tokunagakazuki330@gmail.com>
2018-12-29 23:53:35 +0900

    アップロードファイルを受信する

--- 2/2.1/2.1.2/server.js
+++ 2/2.1/2.1.2/server.js
@@ -1,38 +1,25 @@
 var http = require('http');
-var querystring = require('querystring');
-var util = require('util');
+var formidable = require('formidable');
 var form = require('fs').readFileSync('form.html');
-var maxData = 2 * 1024 * 1024; // 2MB
 
-http.createServer(function(req, res) {
-  // req.methodで、クライアントから送信されたリクエストがGETであるか確認
+http.createServer(function(req, res){
   if(req.method === 'GET'){
-    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.writeHead(200, {'Content-Type':'text/html'});
     res.end(form);
   }
-  // req.methodで、クライアントから送信されたリクエストがPOSTであるか確認
   if(req.method === 'POST'){
-    var postData = '';
-    req.on('data', function(chunk){
-      // POSTデータを受信するたびに、postData変数に送る。
-      postData += chunk;
-      // POSTされたデータの容量が2MBを超えた場合は、413エラーを返す。
-      if(postData.length > maxData){
-        // postDataの内容を破棄
-        postData = '';
-        // this.pause()でストリームの受信を中断。しばらく後に、V8のガベージコレクションでメモリから削除される。
-        // なお、stream.pauseの代わりにstream.destroyを使うと、クライアントにレスポンスを返せなくなる。
-        this.pause();
-        res.writeHead(413); // 413 Request Entity Too Large
-        res.end('POSTデータが大きすぎます。');
-      }
+    // POSTデータを受け取るとインスタンスを生成
+    var incoming = new formidable.IncomingForm();
+    // ファイルをアップロードするディレクトリを指定
+    incoming.uploadDir = 'uploads';
+    // incomingがそれぞれのファイル読み込み終了時に発生させるfileイベントを待機。fileイベントはコールバックにfieldとfile（アップロードファイルの情報を格納したオブジェクト）という2つの引数を渡す。
+    incoming.on('file', function(field, file) {
+      if (!file.size){return;}
+      res.write(file.name + 'を受け取りました\n');
     }).on('end', function(){
-      if(!postData){ res.end(); return; } // postDataが空の場合の処理
-      // querystringモジュールのparseメソッドは、クエリストリング形式のデータをオブジェクトに変換する。
-      var postDataObject = querystring.parse(postData);
-      console.log('ユーザーが次のデータをPOSTしました:\n' + postData);
-      // inspectメソッドは、postDataObjectを簡単に文字列にすることができる。
-      res.end('あなたがPOSTしたデータ:\n' + util.inspect(postDataObject));
+      // multipartメッセージの解析終了時に発行されるendイベントを受け取ると、res.endでレスポンスの構築を終了して送信する
+      res.end('すべてのファイルを受け取りました。');
     });
+    incoming.parse(req);
   }
 }).listen(8080);
