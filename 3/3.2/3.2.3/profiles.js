ffd0653ab95f9e33bd69d7e6fa2ed83d21a345b6
kazukitokunaga <tokunagakazuki330@gmail.com>
2018-12-30 15:04:17 +0900

    まずはファイルのコピー

---
+++ 3/3.2/3.2.3/profiles.js
@@ -0,0 +1,38 @@
+module.exports = {
+  ryan:{
+    name: "Ryan Dahl",
+    irc: "rhah",
+    twitter: "ryah",
+    github: "ry",
+    location:{'$': {city: 'San Francisco',country: 'USA' }},
+    description: "Creator of node.js"
+  }, isaac:{
+    name: "Isaac Schlueter",
+    irc:"isaacs",
+    twitter: "izs",
+    github: "isaacs",
+    location:{'$': {city: 'San Francisco',country: 'USA' }},
+    description: "Author of npm, core contributor"
+  }, bert:{
+    name: "Bert Belder",
+    irc:"piscisaureus",
+    twitter: "piscisaureus",
+    github: "piscisaureus",
+    location:{'$': { country: 'Netherlands' }},
+    description: "Windows support, overall contributor"
+  }, tj:{
+    name: "TJ Holowaychuk",
+    irc:"tjholowaychuk",
+    twitter: "tjholowaychuk",
+    github: "visionmedia",
+    location:{'$': {city: 'Victoria',country: 'Canada' }},
+    description: "Author of express, jade and other popular modules"
+  }, felix:{
+    name: "Felix Geisendorfer",
+    irc:"felixge",
+    twitter: "felixge",
+    github: "felixge",
+    location:{'$': {city: 'Berlin',country: 'Germany' }},
+    description: "Author of formidable, active core developer"
+  },
+};
