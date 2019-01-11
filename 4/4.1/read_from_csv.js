a68b2739855fa14d48f5b5625f38f9581db6b3ba
kazukitokunaga <tokunagakazuki330@gmail.com>
2019-01-11 22:28:13 +0900

    csvファイルを読み込む②

--- 4/4.1/read_from_csv.js
+++ 4/4.1/read_from_csv.js
@@ -1,5 +1,9 @@
 var csv = require('ya-csv');
-var reader = csv.createCsvFileReader('data.csv');
+var reader = csv.createCsvFileReader('custom_data.csv', {
+  'separator': ':',
+  'quote': '|',
+  'escape': '\\'
+});
 var data = [];
 
 reader.on('data', function(rec){
